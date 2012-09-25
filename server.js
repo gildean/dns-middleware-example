8c6924ab180ac66b21113a71eec7e84c23639cd0
OK <gildean@salaliitto.com>
2012-09-25 21:19:07 +0300

    Fix indentation.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express"),
-	http = require('http'),
-	whois = require('dns-middleware'),
-	app = express(),
-	server = http.createServer(app);
+    http = require('http'),
+    whois = require('dns-middleware'),
+    app = express(),
+    server = http.createServer(app);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
@@ -10,15 +10,15 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.get("/*", function (req, res) {
-  res.render('view');
+    res.render('view');
 });
 
 app.post('/*', whois, function (req, res) {
-	res.send(req.data);
+    res.send(req.data);
 });
 
 app.use(function(req, res, next){
-	res.send(420, 'Slow down bro.');
+    res.send(420, 'Slow down bro.');
 });
  
 server.listen(3009);
